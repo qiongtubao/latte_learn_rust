7aacb4efbe055aedfebada0ff985f225abe30bfa
qiongtubao <810776886@qq.com>
2019-04-19 09:34:22 +0800

    添加类型debug格式化 以及自定义格式化输出

--- example/format/src/main.rs
+++ example/format/src/main.rs
@@ -1,3 +1,25 @@
+use std::fmt::{Formatter, Display, Result};
+//自己实现fmt 
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32
+}
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let lat_c = if self.lat >= 0.0 { 'N'  } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+//给struct 追加fmt
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
 fn main() {
     println!("{}", 1);
     println!("{:o}",9);
@@ -10,4 +32,23 @@ fn main() {
     println!("{:?}", "test");
     println!("{:#?}", ("test1", "test2"));
     println!("{a} {b} {b}", a = "x", b = "y");
+
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+
+
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display
+        println!("{:?}", *color);
+    }
 }
