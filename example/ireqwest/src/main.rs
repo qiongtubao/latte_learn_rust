8b740b979236cb59c21a1be61d1c68864fffcfa4
qiongtubao <810776886@qq.com>
2019-06-22 19:03:23 +0800

    add url 的用法

--- example/ireqwest/src/main.rs
+++ example/ireqwest/src/main.rs
@@ -3,6 +3,7 @@ extern crate reqwest;
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
+extern crate url;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -15,13 +16,22 @@ struct Crate {
 struct Content {
     crates: Vec<Crate>,
 }
+fn query(query: &str) -> Result<(), reqwest::Error> {
+    use url::Url;
 
-fn main()  ->  Result<(), reqwest::Error> {
-    let body: Content = reqwest::get("https://crates.io/api/v1/crates?page=1&per_page=10&q=tokio")?
+let url = Url::parse_with_params("https://crates.io/api/v1/crates",
+                                 &[("page", "1"), ("per_page", "10"),("q", query)]).unwrap();
+    // let mut uri = "https://crates.io/api/v1/crates?page=1&per_page=10&q=".to_string();
+    // uri.push_str(query);
+    let body: Content = reqwest::get(&url.into_string())?
     .json()?;
 
     println!("body = {:?}", body);
-    
     Ok(())
+}
+fn main()  ->  Result<(), reqwest::Error> {
+    
+    query("tokio")
+    
 }
 
